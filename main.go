86628345f340eb410825b756d6c92c9258cf4af5
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-05-20 10:48:38 -0300

    Leitura e escrita de arquivos

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 )
 
 func main() {
+
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
--- prices/prices.go
+++ prices/prices.go
@@ -1,21 +1,50 @@
 package prices
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/AntonioGuilhermeDev/pricecalculator/conversion"
+	"github.com/AntonioGuilhermeDev/pricecalculator/filemanager"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
-	TaxIncludedPrices map[string]float64
+	TaxIncludedPrices map[string]string
 }
 
-func (job TaxIncludedPriceJob) Process() {
-	result := make(map[string]float64)
+func (job *TaxIncludedPriceJob) LoadData() {
 
-	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+	lines, err := filemanager.ReadLines("prices.txt")
+
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	fmt.Println(result)
+	prices, err := conversion.StringsToFloats(lines)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	job.InputPrices = prices
+}
+
+func (job *TaxIncludedPriceJob) Process() {
+	job.LoadData()
+
+	result := make(map[string]string)
+
+	for _, price := range job.InputPrices {
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
+	}
+
+	job.TaxIncludedPrices = result
+
+	filemanager.WriteJSON(fmt.Sprintf("result_%.0f.json", job.TaxRate*100), job)
+
 }
 
 func NewTaxIncludePriceJob(taxRate float64) *TaxIncludedPriceJob {
